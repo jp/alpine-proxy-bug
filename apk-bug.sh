f9c3b2a6d1296c6ef9a455ae32b323e03240c57f
Julien Pellet <pelletj@gmail.com>
2021-01-18 18:32:41 -0500

    add curl command

--- apk-bug.sh
+++ apk-bug.sh
@@ -8,6 +8,12 @@ echo "Installing curl without the proxy"
 echo "================================="
 env -i /sbin/apk add curl
 
+
+echo "================================="
+echo "curl through the proxy to show it is working"
+echo "================================="
+curl -vv https://api.chucknorris.io/jokes/random
+
 echo "================================="
 echo "Installing tcpdump with the proxy"
 echo "================================="
